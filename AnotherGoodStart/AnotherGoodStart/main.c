2b53262b8e62c036e54535a180a56c75534e1e1b
Alla <greeniam79@gmail.com>
2016-01-27 16:12:28 +0200

    Add project to branch1

---
+++ AnotherGoodStart/AnotherGoodStart/main.c
@@ -0,0 +1,30 @@
+//
+//  main.c
+//  AnotherGoodStart
+//
+//  Created by greeniam on 09.12.15.
+//  Copyright © 2015 greeniam. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+//float lastTemperature;
+
+
+
+int main(int argc, const char * argv[]) {
+   
+
+    float *startOFBuffer;
+    
+    startOFBuffer = malloc (1000*sizeof(float));
+    free (startOFBuffer);
+    
+    startOFBuffer=NULL; 
+        
+    
+    
+    return 0;
+}
